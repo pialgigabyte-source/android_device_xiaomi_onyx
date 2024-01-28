88daab6df3b5b8f6e3a9dc30affa682f9d39ce26
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-28 21:14:48 +0530

    onyx: Silence some spammy logging
    - HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
      usage up to over 3%. With this change, logs are much cleaner and logd
      idle CPU usage is reduced to under 1%, while still allowing error logs
      to pass through.
    - Do this only in user/debug builds (except eng).
    
    Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>
    Change-Id: I4f84b4d96c8135d00f57e4502c2e1816be2eef55

--- device.mk
+++ device.mk
@@ -264,6 +264,25 @@ $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    MiClstc \
+    MiStcImpl \
+    SDM \
+    SRE \
+    android.hardware.power-service-qti \
+    libsensor-boledalgo \
+    libsensor-parseRGB \
+    libsensor-qshcalapi \
+    sensors \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice-service.aidl
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_tuna_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_tuna_v0.xml \
