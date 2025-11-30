861a5690ddd6c764f846db7737e131bf7d4bec78
LuK1337 <priv.luk@gmail.com>
2025-11-30 09:08:10 +0000

    onyx: Build android.frameworks.sensorservice@1.0.vendor
    - Required by libdpps.
    
    Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- device.mk
+++ device.mk
@@ -358,6 +358,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.xiaomi-multihal \
     sensors.dynamic_sensor_hal \
     sensors.xiaomi.v2
