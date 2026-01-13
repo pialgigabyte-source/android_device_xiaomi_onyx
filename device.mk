3057b0d2d1a03503e6492f2d4670a17d2c25e319
SharmagRit <sharmagritstuff@gmail.com>
2026-01-13 12:51:34 +0000

    onyx: Add BCR support

--- device.mk
+++ device.mk
@@ -120,6 +120,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+# Basic Call Recorder
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
